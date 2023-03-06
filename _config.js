4a4a807d200330ccef1e49d8f5ae94b2e3f92bc5
Evans Oloo <evans.oloo@student.moringaschool.com>
2023-03-06 12:23:51 +0300

    db link created

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://EOLOO:Nairobi@254@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://EOLOO:Nairobi@254@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://EOLOO:Nairobi@254@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
