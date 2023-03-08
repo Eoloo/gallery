2328fe52cf46e6db56162dfd9cbf9ac2401b07fb
Evans Oloo <evans.oloo@student.moringaschool.com>
2023-03-08 21:57:08 +0300

    updated mongo url

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Testing auto-deploy
+Updated mongodb url
--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb://eoloo:Nairobi254@ac-v7zqbdy-shard-00-00.ha1qbwy.mongodb.net:27017,ac-v7zqbdy-shard-00-01.ha1qbwy.mongodb.net:27017,ac-v7zqbdy-shard-00-02.ha1qbwy.mongodb.net:27017/?ssl=true&replicaSet=atlas-wduxz7-shard-0&authSource=admin&retryWrites=true&w=majority',
+    development: 'mongodb://eoloo:Nairobi254@ac-v7zqbdy-shard-00-00.ha1qbwy.mongodb.net:27017,ac-v7zqbdy-shard-00-01.ha1qbwy.mongodb.net:27017,ac-v7zqbdy-shard-00-02.ha1qbwy.mongodb.net:27017/?ssl=true&replicaSet=atlas-wduxz7-shard-0&authSource=admin&retryWrites=true&w=majority',
+    test: 'mongodb://eoloo:Nairobi254@ac-v7zqbdy-shard-00-00.ha1qbwy.mongodb.net:27017,ac-v7zqbdy-shard-00-01.ha1qbwy.mongodb.net:27017,ac-v7zqbdy-shard-00-02.ha1qbwy.mongodb.net:27017/?ssl=true&replicaSet=atlas-wduxz7-shard-0&authSource=admin&retryWrites=true&w=majority',
 }
 module.exports = config;
