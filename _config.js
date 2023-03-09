4c9c2863415587786ab1de6970f69807b34e5e78
Evans Oloo <evans.oloo@student.moringaschool.com>
2023-03-09 22:34:19 +0300

    db updated

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://eoloo:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://EOLOO:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://EOLOO:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://EOLOO:Nairobi254@cluster0.ha1qbwy.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
